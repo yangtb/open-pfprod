45ddc3cb36bb73409dc00e4743a5d70eb29f93b2
yangtongbin <yang_t_b@qq.com>
2018-09-22 22:51:55 +0800

    增加@Override注解

--- open-pfprod-web/src/main/java/com/sm/pfprod/web/security/PfAbstractAuthenticationProcessingFilter.java
+++ open-pfprod-web/src/main/java/com/sm/pfprod/web/security/PfAbstractAuthenticationProcessingFilter.java
@@ -67,6 +67,7 @@ public abstract class PfAbstractAuthenticationProcessingFilter extends GenericFi
 		Assert.notNull(authenticationManager, "AuthenticationManager必须明确定义");
 	}
 
+	@Override
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 		HttpServletRequest request = (HttpServletRequest) req;
 		HttpServletResponse response = (HttpServletResponse) res;
