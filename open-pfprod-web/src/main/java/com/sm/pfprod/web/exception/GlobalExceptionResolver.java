59561ae1dcdfbba7dcb01d5605ab305b1b8a3405
yangtongbin <yang_t_b@qq.com>
2018-10-04 22:39:07 +0800

    打印错误日志

--- open-pfprod-web/src/main/java/com/sm/pfprod/web/exception/GlobalExceptionResolver.java
+++ open-pfprod-web/src/main/java/com/sm/pfprod/web/exception/GlobalExceptionResolver.java
@@ -4,6 +4,8 @@ import com.sm.open.care.core.ErrorCode;
 import com.sm.open.care.core.ErrorMessage;
 import com.sm.open.care.core.ResultObject;
 import com.sm.open.care.core.exception.BizRuntimeException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -20,6 +22,9 @@ import javax.servlet.http.HttpServletRequest;
 @ControllerAdvice
 public class GlobalExceptionResolver {
 
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionResolver.class);
+
     @ResponseBody
     @ExceptionHandler(Exception.class)
     public ResultObject GlobalExceptionResolver(HttpServletRequest request, Exception e) {
@@ -35,7 +40,8 @@ public class GlobalExceptionResolver {
             }
             // 无权限异常抛到上层
             throw new AccessDeniedException(e.getMessage());
-        }else {
+        } else {
+            LOGGER.info("【GlobalExceptionResolver】服务器内部错误, url:{}, msg:{}", requestUrl, e.getMessage());
             return ResultObject.create(requestUrl, ErrorCode.ERROR_SYS_160002, ErrorMessage.MESSAGE_SYS_160002);
         }
     }
