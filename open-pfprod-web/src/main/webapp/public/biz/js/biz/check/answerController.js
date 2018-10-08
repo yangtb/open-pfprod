18f406eafc1f767e77e24e13ba663f9b909637cb
yangtongbin <yang_t_b@qq.com>
2018-10-08 19:52:42 +0800

    add检查检验服务

--- open-pfprod-web/src/main/webapp/public/biz/js/biz/check/answerController.js
+++ open-pfprod-web/src/main/webapp/public/biz/js/biz/check/answerController.js
@@ -18,12 +18,12 @@ layui.config({
         , cols: [[
             {type: 'radio'},
             {field: 'fgActive', minWidth: 73, title: '状态', templet: '#fgActiveTpl'},
-            {field: 'desAnswer', minWidth: 150, title: '答案内容'},
-            {fixed: 'right', title: '操作', align: 'center', toolbar: '#inquisitionAnswer'}
+            {field: 'desResult', minWidth: 150, title: '结果内容'},
+            {fixed: 'right', title: '操作', align: 'center', toolbar: '#checkAnswer'}
         ]] //设置表头
-        , url: basePath + '/pf/p/inquisition/question/answer/list'
+        , url: basePath + '/pf/p/check/question/answer/list'
         , where: {
-            idInques: idInques,
+            idBody: idBody,
         }
         , page: false
     });
@@ -44,7 +44,6 @@ layui.config({
         , url: basePath + '/upload'
         , field: 'file'
         , accept: 'file' //普通文件
-        //obj参数包含的信息，跟 choose回调完全一致，可参见上文。
         , before: function (obj) {
             $('#path').hide();
             $('#uploadProgress').show();
@@ -58,15 +57,13 @@ layui.config({
                     url: basePath + '/selectUploadPercent',
                     dataType: "json",
                     success: function (data) {
-                        console.log(data);
                         var per = data.percent + "%";
                         element.progress('demo', per);
                     }, error: function (data) {
-                        console.log(data);
                         alert("ajax异常！！！");
                     }
                 });
-            }, 100);
+            }, 500);
         }
         , done: function (res) {
             $('#path').show();
@@ -74,12 +71,13 @@ layui.config({
             clearInterval(timer);
             $('#path').val(res.data.path);
             $('#idMedia').val(res.data.idMedia);
+            $('#sdType').val(res.data.sdType);
             clearPercent();
-            console.log(res)
         }
         , error: function () {
             $('#path').show();
             $('#uploadProgress').hide();
+            $('#sdType').val('');
             clearInterval(timer);
             clearPercent();
         }
@@ -96,7 +94,6 @@ layui.config({
             dataType: "json",
             success: function (data) {
                 element.progress('demo', 0);
-                console.log("ddd:" + JSON.stringify(data));
             },
         });
     };
@@ -125,9 +122,9 @@ layui.config({
             };
             common.openPhoto(json)
         } else if (sdType == '2') {
-            common.openAudio("http://hsknowledgebase.oss-cn-hangzhou.aliyuncs.com/mytest/music");
+            common.openAudio(path.substring(0, path.lastIndexOf(".")));
         } else if (sdType == '3') {
-            common.openTopVideo(path, basePath + '/video/form', 890, 504);
+            common.openTopVideo(basePath + '/video/form?path=' + path, 890, 504);
         } else {
             layer.tips("该文件类型暂不支持预览", '#preview', {tips: 1});
         }
@@ -143,17 +140,20 @@ layui.config({
         if (!data.field.fgActive) {
             data.field.fgActive = '0';
         }
-        data.field.fgReason = data.field.radioGroup === 'fgReason' ? '1' : '0';
-        data.field.fgBack = data.field.radioGroup === 'fgBack' ? '1' : '0';
-        data.field.fgTag = data.field.radioGroup === 'fgTag' ? '1' : '0';
-        data.field.idInques = idInques;
-        common.commonPost(basePath + '/pf/r/inquisition/question/answer/save', data.field, '保存');
-        $('#reset').click();
-        _tableReload();
+        data.field.fgReason = data.field.fgReason ? '1' : '0';
+        data.field.fgBack = data.field.fgBack ? '1' : '0';
+        data.field.fgTag = data.field.fgTag ? '1' : '0';
+        data.field.idBody = idBody;
+        common.commonPost(basePath + '/pf/r/check/question/answer/save', data.field, '保存', '', _callBack);
         return false;
     });
 
-//监听工具条
+    var _callBack = function (data) {
+        _tableReload();
+        $('#idResult').val(data.data);
+    }
+
+    //监听工具条
     table.on('tool(answerTableFilter)', function (obj) {
         var data = obj.data;
         if (obj.event === 'del') {
@@ -162,31 +162,31 @@ layui.config({
     });
 
     var _delAnswer = function (currentData) {
-        var url = basePath + '/pf/r/inquisition/question/answer/del';
+        var url = basePath + '/pf/r/check/question/answer/del';
         var reqData = new Array();
-        var name = '【' + currentData.desAnswer + '】';
-        reqData.push(currentData.idAnswer);
+        var name = '【' + currentData.desResult + '】';
+        reqData.push(currentData.idResult);
         var data = {};
         data.list = reqData;
         data.status = '1';
 
-        layer.confirm('真的要删除答案内容：' + name + '么？', {
-            title: '删除答案内容提示',
+        layer.confirm('真的要删除结果内容：' + name + '么？', {
+            title: '删除结果内容提示',
             resize: false,
             btn: ['确定', '取消'],
             btnAlign: 'c',
             icon: 3
         }, function (index) {
-            common.commonPost(url, data, '删除');
+            common.commonPost(url, data, '删除', '', _tableReload);
+            $('#reset').click();
             layer.closeAll(index);
-            _tableReload();
         })
     };
 
     var _tableReload = function () {
         table.reload('answerTableId', {
             where: {
-                idInques: idInques,
+                idBody: idBody,
             },
             height: 'full-30'
         });
@@ -195,13 +195,6 @@ layui.config({
     table.on('radio(answerTableFilter)', function (obj) {
         if (!obj.data.desExpert) {
             obj.data.desExpert = "";
-        }
-        if (obj.data.fgReason == '1') {
-            obj.data.radioGroup = 'fgReason';
-        } else if (obj.data.fgBack == '1') {
-            obj.data.radioGroup = 'fgBack';
-        } else if (obj.data.fgTag == '1') {
-            obj.data.radioGroup = 'fgTag';
         }
         $('#reset').click();
         $("#answerForm").autofill(obj.data);
