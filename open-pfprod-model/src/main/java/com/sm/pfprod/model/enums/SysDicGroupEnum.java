21caca324f15ed458fc075ee799c653aa2d2fd1f
yangtongbin <yang_t_b@163.com>
2018-10-11 10:10:20 +0800

    add enum

--- open-pfprod-model/src/main/java/com/sm/pfprod/model/enums/SysDicGroupEnum.java
+++ open-pfprod-model/src/main/java/com/sm/pfprod/model/enums/SysDicGroupEnum.java
@@ -13,7 +13,9 @@ public enum SysDicGroupEnum {
     BANNER_POSITION("BANNER_POSITION", "banner位"),
     EXAM_WAYS("EXAM_WAYS", "检查方式"),
     BODY_POSITION("BODY_POSITION", "检查题库"),
-    CLINIC_TEMPLATE_TAG("CLINIC_TEMPLATE_TAG", "模板标签");
+    CLINIC_TEMPLATE_TAG("CLINIC_TEMPLATE_TAG", "模板标签"),
+    CASE_HISTORY_LEVEL("CASE_HISTORY_LEVEL", "病历级别"),
+    CASE_HISTORY_USE("CASE_HISTORY_USE", "病历用途");
 
     private String code;
     private String desc;
