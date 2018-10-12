9640369aae32a6a72bcb3ef88d68091c0b3e36d1
yangtb <yang_t_b@163.com>
2018-10-12 11:24:09 +0800

    全屏功能

--- open-pfprod-web/src/main/webapp/public/biz/js/index/indexController.js
+++ open-pfprod-web/src/main/webapp/public/biz/js/index/indexController.js
@@ -64,24 +64,34 @@ layui.config({
         var msg;
         if (moment(gmtValid).isBefore(new Date())) {
             msg = "您的平台试用已到期！";
-        }  else {
-            msg= '您的平台试用将于' + moment(gmtValid).format('YYYY月MM月DD日') +'到期！';
+        } else {
+            msg = '您的平台试用将于' + moment(gmtValid).format('YYYY月MM月DD日') + '到期！';
         }
         if (moment(gmtValid).isBefore(moment().add(orgExpiryNoticeDay, 'days').calendar())) {
             layer.open({
                 type: 1
-                ,title: false //不显示标题栏
-                ,closeBtn: false
-                ,area: '300px;'
-                ,shade: 0.8
-                ,id: 'notice_org_expired' //设定一个id，防止重复弹出
-                ,btn: ['火速续期', '残忍拒绝']
-                ,btnAlign: 'c'
-                ,moveType: 1 //拖拽模式，0或者1
-                ,content: '<div style="padding: 50px; line-height: 22px; background-color: #393D49; color: #fff; font-weight: 300;">' +
-                    '尊敬的用户：<br><br>' + msg +'<br><br>' +
+                ,
+                title: false //不显示标题栏
+                ,
+                closeBtn: false
+                ,
+                area: '300px;'
+                ,
+                shade: 0.8
+                ,
+                id: 'notice_org_expired' //设定一个id，防止重复弹出
+                ,
+                btn: ['火速续期', '残忍拒绝']
+                ,
+                btnAlign: 'c'
+                ,
+                moveType: 1 //拖拽模式，0或者1
+                ,
+                content: '<div style="padding: 50px; line-height: 22px; background-color: #393D49; color: #fff; font-weight: 300;">' +
+                    '尊敬的用户：<br><br>' + msg + '<br><br>' +
                     '为了不影响您的使用，请您尽快申请续期 ^_^</div>'
-                ,success: function(layero){
+                ,
+                success: function (layero) {
                     //var btn = layero.find('.layui-layer-btn');
                     $('.layui-layer-btn0').on('click', function () {
                         $('#orgInfo').click();
@@ -105,6 +115,49 @@ layui.config({
                 }
             }
         });
-    };
+    }
+
+
+    $('#fullScreen').on('click', function () {
+        $("#exitScreen").css("display","block");
+        $("#fullScreen").css("display","none");
+        fullScreen();
+    });
+
+    $('#exitScreen').on('click', function () {
+        $("#exitScreen").css("display","none");
+        $("#fullScreen").css("display","block");
+        exitScreen();
+    });
+
+
+    //全屏
+    function fullScreen() {
+        var el = document.documentElement;
+        var rfs = el.requestFullScreen || el.webkitRequestFullScreen || el.mozRequestFullScreen || el.msRequestFullscreen;
+        if (typeof rfs != "undefined" && rfs) {
+            rfs.call(el);
+        }
+        return;
+    }
+
+    //退出全屏
+    function exitScreen() {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        }
+        else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        }
+        else if (document.webkitCancelFullScreen) {
+            document.webkitCancelFullScreen();
+        }
+        else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+        if (typeof cfs != "undefined" && cfs) {
+            cfs.call(el);
+        }
+    }
 
 });
