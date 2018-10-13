af442f10aebe5f9575ce28b6db0e63d5d58eb694
yangtb <yang_t_b@163.com>
2018-10-13 15:12:23 +0800

    租店分组增加分页功能

--- open-pfprod-web/src/main/webapp/public/biz/js/system/dic/dicController.js
+++ open-pfprod-web/src/main/webapp/public/biz/js/system/dic/dicController.js
@@ -21,8 +21,16 @@ layui.config({
             {fixed: 'right', title: '操作', width: 85, align: 'center', toolbar: '#dicBar'}
         ]] //设置表头
         , url: basePath + '/pf/p/dic/list'
-        //, even: true
-        , page: false
+        , limit: 15
+        , even: true
+        , page: {//支持传入 laypage 组件的所有参数（某些参数除外，如：jump/elem） - 详见文档
+            layout: ['limit', 'count', 'prev', 'page', 'next'] //自定义分页布局
+            //,curr: 5 //设定初始在第 5 页
+            , groups: 1 //只显示 1 个连续页码
+            , first: false //不显示首页
+            , last: false //不显示尾页
+            , limits: [15, 30, 100]
+        }
     });
 
     //执行渲染
