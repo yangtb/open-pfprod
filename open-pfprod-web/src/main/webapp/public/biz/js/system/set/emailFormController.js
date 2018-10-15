902c18764e38019b0a4991b5d39861643f5b64a3
yangtb <yang_t_b@163.com>
2018-10-15 20:44:57 +0800

    富文本调整

--- open-pfprod-web/src/main/webapp/public/biz/js/system/set/emailFormController.js
+++ open-pfprod-web/src/main/webapp/public/biz/js/system/set/emailFormController.js
@@ -3,10 +3,27 @@
  */
 layui.config({
     base: basePath + '/public/layui/build/js/'
-}).use(['form', 'layer', 'jquery', 'common'], function () {
+}).use(['form', 'layer', 'jquery', 'layedit', 'common'], function () {
     var $ = layui.$
         , form = layui.form
+        , layedit = layui.layedit
         , common = layui.common;
+
+    layedit.build('emailContent', {
+        tool: [
+        'strong' //加粗
+        ,'italic' //斜体
+        ,'underline' //下划线
+        ,'del' //删除线
+        ,'|' //分割线
+        ,'left' //左对齐
+        ,'center' //居中对齐
+        ,'right' //右对齐
+        ,'link' //超链接
+        ,'unlink' //清除链接
+        ,'face' //表情
+    ]
+    });
 
     form.on('submit(addEmail)', function (data) {
         var url = basePath + '/pf/r/set/email';
